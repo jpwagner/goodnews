13019e4ee81b0239dd51b02e3d5ce1478a90c92e
JP Wagner <jpwagner@mit.edu>
2017-05-23 11:22:51 -0400

    allow fields to be nullable

--- goodnews/models.py
+++ goodnews/models.py
@@ -9,8 +9,8 @@ class Article(SuperModel):
 	author = models.CharField(max_length=255, null=True, blank=True)
 	description = models.TextField(null=True, blank=True)
 	url = models.URLField(max_length=1000, null=True, blank=True)
-	score = models.FloatField(default=0)
-	published_at = models.DateTimeField()
+	score = models.FloatField(null=True, blank=True)
+	published_at = models.DateTimeField(null=True, blank=True)
 
 	def __unicode__(self):
 		return self.title
